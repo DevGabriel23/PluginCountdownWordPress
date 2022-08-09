c0ea3c8fcca35b107a2a3331c4074c4dd4cab059
CesarSanchezdev <trasforce@outlook.com>
2022-08-09 14:21:37 -0500

    Conexión a base de datos mysql

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost= 'localhost';
+$databaseName= 'wordpress';
+$databaseUsername= 'root';
+$databasePassword= '';
+
+$mysqli = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+?>
