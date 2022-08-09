8236f90254c913d7d3f5de758c4122c891d5bd37
alejandromotadev <aamotadev@gmail.com>
2022-08-09 10:06:40 -0500

    Primer prueba de calculo del tiempo restante
    Co-Authored-By: Gabriel Ramón <103237220+Gabriel-Ramon@users.noreply.github.com>

--- code.php
+++ code.php
@@ -1,3 +1,35 @@
 <?php
+/**
+ * Plugin Name
+ *
+ * @package           CountdownPluginIDS
+ * @author            Alejandro Mota, Gabriel Ramon, Cesar Sanchez, Marisol Solis
+ * @copyright         2022 HighBits
+ *
+ * @wordpress-plugin
+ * 
+ * Plugin Name:       CountdownIDS
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ */
 
+/*** Countdown Timer Shortcode ***/
+$day = localtime();
+
+$waiting_day = 1660056724;
+$time_left = $waiting_day - time();
+
+$days = floor($time_left / (60*60*24));
+$time_left %= (60 * 60 * 24);
+
+$hours = floor($time_left / (60 * 60));
+$time_left %= (60 * 60);
+
+$min = floor($time_left / 60);
+$time_left %= 60;
+
+$sec = $time_left;
+
+echo "DAY: $day[3]";
+echo "Remaing time: $days days and $hours hours and $min min and $sec sec left";
 ?>
